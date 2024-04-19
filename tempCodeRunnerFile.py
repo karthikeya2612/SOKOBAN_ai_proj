00dc9e64e13f8fb014d235f0db01196f822a2591
Karthikeyacs33 <karthikeyamaruvada26@gmail.com>
2024-04-19 22:58:48 +0530

    sokoban ai_proj

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+self.load_game_txt_surface = self.font.render(
+        #     self.load_game_txt, True, SOKOBAN.BLACK, SOKOBAN.WHITE)
+        # self.load_game_txt_position = (
+        #     (SOKOBAN.WINDOW_WIDTH / 2) - (self.load_game_txt_surface.get_width() / 2), 370)
+        # window.blit(self.load_game_txt_surface, self.load_game_txt_position)
